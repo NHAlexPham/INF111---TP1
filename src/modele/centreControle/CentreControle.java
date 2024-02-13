b050cdeb0446d733d72b0f3ea2b039dbd207a41a
Gouloum22 <159480450+Gouloum22@users.noreply.github.com>
2024-02-13 11:30:55 -0500

    Update CentreControle.java
    update avec commentaire de la classe

--- src/modele/centreControle/CentreControle.java
+++ src/modele/centreControle/CentreControle.java
@@ -1,28 +1,41 @@
 package modele.centreControle;
 
+import java.util.ArrayList;
+
 import modele.communication.Message;
 import modele.satelliteRelai.SatelliteRelai;
+import utilitaires.FileSimplementChainee;
 
 public class CentreControle extends modele.communication.TransporteurMessage{
 
 	private SatelliteRelai satelliteRelai;
+	private FileSimplementChainee msgEnvoye = new FileSimplementChainee();
+	private ArrayList<Message> msgRecu = new ArrayList<>();
 	
 	public CentreControle(SatelliteRelai satelliteRelai){
 		
 		this.satelliteRelai = satelliteRelai;
 	}
 	
+	
+	public FileSimplementChainee getMsgEnvoye() {
+		return msgEnvoye;
+	}
+	
+	
 	@Override
 	protected void envoyerMessage(Message msg) {
 
-		satelliteRelai.envoyerMessageVersRover(msg);
+		satelliteRelai.envoyerMessageVersRover(msg);	//envoie le message au Rover
+		msgEnvoye.ajouterElement(msg);					//store le message dans la liste des messages envoyes du centre de controle
 		
 	}
 
 	@Override
 	protected void gestionnaireMessage(Message msg) {
-
-		System.out.println(this.getClass().getName() + msg);
+		
+		System.out.println(this.getClass().getName() + msg.getCompte()); //affiche le nom de la classe est le numero du message
+		
 		
 	}
 
