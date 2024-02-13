ca6445cc3872ff7caa55be65c913944e4d53ceba
Gouloum22 <159480450+Gouloum22@users.noreply.github.com>
2024-02-13 11:31:15 -0500

    Update Rover.java

--- src/modele/rover/Rover.java
+++ src/modele/rover/Rover.java
@@ -1,28 +1,41 @@
 package modele.rover;
 
+import java.util.ArrayList;
+
 import modele.communication.Message;
 import modele.satelliteRelai.SatelliteRelai;
+import utilitaires.FileSimplementChainee;
 
 public class Rover extends modele.communication.TransporteurMessage{
 
 	private SatelliteRelai satelliteRelai;
+	private FileSimplementChainee msgEnvoye = new FileSimplementChainee();
+	private ArrayList<Message> msgRecu = new ArrayList<>();
 	
 	public Rover(SatelliteRelai satelliteRelai){
 		
 		this.satelliteRelai = satelliteRelai;
 	}
 	
+	
+	public FileSimplementChainee getMsgEnvoye() {
+		return msgEnvoye;
+	}
+	
+	
 	@Override
 	protected void envoyerMessage(Message msg) {
 
-		satelliteRelai.envoyerMessageVersRover(msg);
+		satelliteRelai.envoyerMessageVersCentrOp(msg);	//envoie le message vers le Centre de controle
+		msgEnvoye.ajouterElement(msg);					//store le message dans les messages envoyes du Rover
 		
 	}
 
 	@Override
 	protected void gestionnaireMessage(Message msg) {
-
-		System.out.println(this.getClass().getName() + msg);
+		
+		System.out.println(this.getClass().getName() + msg.getCompte());	//affiche le nom de la classe est le numero du message
+		
 		
 	}
 
